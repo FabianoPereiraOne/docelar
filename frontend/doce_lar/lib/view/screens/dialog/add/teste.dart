8f38f861bea78305957212ac3eba239628b66bf0
Vinicius Duarte <105808434+ViniDuarte1@users.noreply.github.com>
2024-11-13 18:37:55 -0300

    Teste
    Teste de download de imagem

--- frontend/doce_lar/lib/view/screens/dialog/add/teste.dart
+++ frontend/doce_lar/lib/view/screens/dialog/add/teste.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:doce_lar/controller/interceptor_dio.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
 
 class UploadScreen extends StatefulWidget {
   @override
@@ -17,13 +19,26 @@ class _UploadScreenState extends State<UploadScreen> {
   File? _image;
   final ImagePicker _picker = ImagePicker();
   late UploadRepository _uploadRepository;
+  List<Document> _uploadedImages = [];
 
   @override
   void initState() {
+    super.initState();
     final loginProvider = Provider.of<LoginController>(context, listen: false);
     final customDio = CustomDio(loginProvider, context);
-    super.initState();
     _uploadRepository = UploadRepository(customDio);
+    _fetchUploadedImages();
+  }
+
+  Future<void> _fetchUploadedImages() async {
+    try {
+      final images = await _uploadRepository.fetchDocuments(); // Assume que esse método existe no repositório
+      setState(() {
+        _uploadedImages = images;
+      });
+    } catch (e) {
+      log('Erro ao carregar imagens: $e');
+    }
   }
 
   Future<void> _selectImage() async {
@@ -46,9 +61,29 @@ class _UploadScreenState extends State<UploadScreen> {
       );
       await _uploadRepository.uploadDocument(document);
       log('Upload realizado com sucesso');
+      _fetchUploadedImages(); // Atualizar a lista de imagens após upload
     } else {
       log('Nenhuma imagem para enviar');
     }
+  }
+
+  Future<void> _downloadImage(String imageUrl) async {
+    try {
+      Dio dio = Dio();
+      var response = await dio.download(imageUrl, await _getDownloadPath(imageUrl));
+      if (response.statusCode == 200) {
+        log('Imagem baixada com sucesso!');
+      } else {
+        log('Falha no download');
+      }
+    } catch (e) {
+      log('Erro ao fazer o download: $e');
+    }
+  }
+
+  Future<String> _getDownloadPath(String imageUrl) async {
+    final directory = await getExternalStorageDirectory();
+    return '${directory!.path}/${imageUrl.split('/').last}';
   }
 
   @override
@@ -78,9 +113,56 @@ class _UploadScreenState extends State<UploadScreen> {
               onPressed: _uploadImage,
               child: Text('Enviar Imagem'),
             ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: _uploadedImages.isNotEmpty
+                  ? GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                      ),
+                      itemCount: _uploadedImages.length,
+                      itemBuilder: (context, index) {
+                        final document = _uploadedImages[index];
+                        String imageUrl = 'http://patrick.vps-kinghost.net:7001${document.key!}';
+                        return GestureDetector(
+                          onTap: () {
+                            _showImagePreview(context, imageUrl);
+                          },
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    )
+                  : Text('Nenhuma imagem enviada.'),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showImagePreview(BuildContext context, String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(imageUrl),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _downloadImage(imageUrl),
+                child: Text('Baixar Imagem'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
